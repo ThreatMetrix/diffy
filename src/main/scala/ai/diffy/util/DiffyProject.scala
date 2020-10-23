45f7f50a2559c19a8fa44a65d6bfaba9928f35ae
Caleb Moore <cmoore@threatmetrix.com>
2020-10-23 15:37:09 +1100

    Remove phone home

--- src/main/scala/ai/diffy/util/DiffyProject.scala
+++ src/main/scala/ai/diffy/util/DiffyProject.scala
@@ -9,18 +9,10 @@ import com.twitter.util.Future
 
 object DiffyProject {
   private[this] sealed trait config {
-    val client: Request => Future[Response]
   }
   private[this] object production extends config {
-    override val client: Request => Future[Response] =
-      Http.client
-        .withTls("diffyproject.appspot.com")
-        .newService("diffyproject.appspot.com:443")
   }
   private[this] object development extends config {
-    override val client: Request => Future[Response] =
-      Http.client
-        .newService("localhost:7000")
   }
 
   private[this] val cfg: config = production
@@ -37,9 +29,5 @@ object DiffyProject {
   private[this] var uid :Map[String, Any] = Map.empty
 
   def log(message: String): Unit = {
-    val request = Request(Method.Post, "/stats")
-    request.setContentTypeJson()
-    request.setContentString(JsonLifter.encode(uid.updated("message", message)))
-    cfg.client(request)
   }
 }
